5a7a90a3e9e01a13fe7a153f5a598688a8fab9a9
VIGNESH <117816170+Vignesh-397@users.noreply.github.com>
2023-11-20 23:16:24 +0530

    Connected to backend(Firebase)

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -86,6 +86,7 @@ class _NewItemState extends State<NewItem> {
                   _enteredName = value!;
                 },
               ),
+              
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
